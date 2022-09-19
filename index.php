fa06d89415c700ecc58236051f2931472028fa10
gurpreetsingh-hub <a21gursinsin@inspedralbes.cat>
2022-09-19 12:58:53 +0200

    mejorar

--- index.php
+++ index.php
@@ -3,45 +3,9 @@
     <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
     <title>Llibres JSON</title>
-    <link rel="stylesheet" href="/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <link rel="stylesheet" href="style.scss">
 
-      <style>
-      #tbstyle {
-        font-family: Arial, Helvetica, sans-serif;
-        border-collapse: collapse;
-        width: 50%;
-      }
-
-      #tbstyle td, #tbstyle th {
-        border: 3px solid transparent;
-        padding: 8px;
-        vertical-align: middle;
-        text-align: center;
-      }
-      #tbstyle th {
-
-      }
-
-
-      #tbstyle tr:nth-child(even){background-color: #f2f2f2;}
-
-      #tbstyle tr:hover {background-color:#ddd;}
-
-      #tbstyle th {
-        padding-top: 12px;
-        padding-bottom: 12px;
-        text-align: center;
-        color: white;
-        vertical-align: middle;
-      }
-      h3{
-        text-align: center;
-        font-weight: bold;
-      }
-      </style>
     </head>
 	  <body>
       <?php
@@ -50,9 +14,7 @@
         $filename = 'books.json';
         $data = file_get_contents($filename); //data read from json file
         $users = json_decode($data);  //decode a data
-         $message = "<h3 class='text-success'>JSON file data</h3>";
-      }else{
-         $message = "<h3 class='text-danger'>JSON file Not found</h3>";
+         $message = "<h1>Fitxer de JSON</h1>";
       }
   ?>
 
@@ -63,9 +25,9 @@
 			 {
 				  echo $message;
 			 ?>
-        <table class="table table-hover" id="tbstyle" >
+        <table class="rwd-table">
           <tbody>
-            <tr class="table-dark">
+            <tr>
               <th>Author</th>
               <th>Country</th>
               <th>ImageLink</th>
